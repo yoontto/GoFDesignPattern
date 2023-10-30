c54b9e0290748600a712535bcd3fb82a12df92dc
yoontto <jedney69@gmail.com>
2023-10-31 00:09:16 +0900

    2장 : Adapter 패턴(2) 위임을 사용한 예제

--- src/main/java/org/example/Adapter/Print.java
+++ src/main/java/org/example/Adapter/Print.java
@@ -3,7 +3,7 @@ package org.example.Adapter;
 /**
  * 필요로 하는 인터페이스
  */
-public interface Print {
+public abstract class Print {
     public abstract void printWeak();
     public abstract void printStrong();
 }
--- src/main/java/org/example/Adapter/PrintBanner.java
+++ src/main/java/org/example/Adapter/PrintBanner.java
@@ -1,22 +1,26 @@
 package org.example.Adapter;
 
 /**
- * 어댑터 역할
- * : 준비된 Banner를 확장(extend)하고, Print 인터페이스 구현(implement)
+ * 어댑터 역할 2
+ * : Banner를 필드로 가지고 생성자를 호출할 떄, 인스턴스를 만든다.
+ * 만든 인스턴스를 이용해 PrintBanner의 메소드가 처리해야 할 일을
+ * Banner의 메소드에게 위임한다.
  */
-public class PrintBanner extends Banner implements Print {
+public class PrintBanner extends Print {
+    private Banner banner;
+
 
     public PrintBanner(String string) {
-        super(string);
+        this.banner = new Banner(string);
     }
 
     @Override
     public void printWeak() {
-        showWithParen();
+        banner.showWithParen();
     }
 
     @Override
     public void printStrong() {
-        showWithAster();
+        banner.showWithAster();
     }
 }
