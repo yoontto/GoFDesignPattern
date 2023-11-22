f36bf5da8ec18d8999c2edd0420fd8a382051341
yoontto <jedney69@gmail.com>
2023-11-22 12:53:29 +0900

    9장 Bridge 패턴 연습문제 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.bridge.CountDisplay;
 import org.example.bridge.Display;
+import org.example.bridge.RandomDisplay;
 import org.example.bridge.StringDisplayImpl;
 
 public class Main {
@@ -9,9 +10,12 @@ public class Main {
         Display d1 = new Display(new StringDisplayImpl("Hello, Korea!"));
         Display d2 = new CountDisplay(new StringDisplayImpl("Hello, World!"));
         CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+        RandomDisplay d4 = new RandomDisplay(new StringDisplayImpl("TEST!"));
+
         d1.display();
         d2.display();
         d3.display();
         d3.multiDisplay(5);
+        d4.randomDisplay(2);
     }
 }
